e2b4ae960da7b785693d3beda736c041f32e0182
Payoja Muduli <72397937+menakinako@users.noreply.github.com>
2021-08-28 23:08:56 +0530

    Lgm task1

---
+++ task-1/script.js
@@ -0,0 +1,25 @@
+let thumb = document.getElementsByClassName('thumb')
+
+let Images = document.getElementsByClassName('active')
+
+for (var i=0; i < thumb.length; i++)
+{
+
+  thumb[i].addEventListener('mouseover', function(){
+    console.log(Images)
+
+    document.getElementById('featured').src = this.src
+  })
+}
+
+
+let buttonRight = document.getElementById('slideRight');
+let buttonLeft = document.getElementById('slideLeft');
+
+buttonLeft.addEventListener('click', function(){
+  document.getElementById('slider').scrollLeft -= 60
+})
+
+buttonRight.addEventListener('click', function(){
+  document.getElementById('slider').scrollLeft += 60
+})
